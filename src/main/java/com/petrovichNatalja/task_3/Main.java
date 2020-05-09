5624448d53ef54fa2d50f3f16d1046efbe6fb44c
NataljaPetrovich <61056228+NataljaPetrovich@users.noreply.github.com>
2020-05-09 15:14:33 +0300

    task 3

---
+++ src/main/java/com/petrovichNatalja/task_3/Main.java
@@ -0,0 +1,9 @@
+package main.java.com.petrovichNatalja.task_3;
+
+public class Main {
+    public static void main(String[] args) {
+        StringAnalyser task3 = new StringAnalyser();
+        task3.countWords();
+        task3.showSortedWords();
+    }
+}
